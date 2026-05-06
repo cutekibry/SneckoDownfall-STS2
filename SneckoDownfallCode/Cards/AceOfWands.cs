033a731d981d2f19afe7ce70566949fed3308604
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-07 00:53:27 +0800

    fix: fix `AceOfWands.cs` to adapt new changes

--- SneckoDownfallCode/Cards/AceOfWands.cs
+++ SneckoDownfallCode/Cards/AceOfWands.cs
@@ -1,6 +1,9 @@
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
 using SneckoDownfall.SneckoDownfallCode.Powers;
 using SneckoDownfall.SneckoDownfallCode.Utils;
 
@@ -8,15 +11,16 @@ namespace SneckoDownfall.SneckoDownfallCode.Cards;
 
 public class AceOfWands : SneckoDownfallCard
 {
+    public override Func<CardModel, bool>? GiftFilter => GiftSelector.IsDebuff;  
     public AceOfWands() : base(3, CardType.Power, CardRarity.Rare, TargetType.Self)
     {
-        WithPower<AceOfWandsPower>(4);
+        WithPower<AceOfWandsPower>(4, false);
         WithKeyword(CardKeyword.Ethereal, UpgradeType.Remove);
-        WithGift(GiftSelector.IsDebuff);
+        WithTip(StaticHoverTip.Block);
     }
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay play)
     {
-        await CommonActions.CardAttack(this, play, DynamicVars["CalculatedHits"].IntValue).Execute(ctx);
+        await PowerCmd.Apply<AceOfWandsPower>(ctx, Owner.Creature, DynamicVars["AceOfWandsPower"].BaseValue, Owner.Creature, this);
     }
 }
