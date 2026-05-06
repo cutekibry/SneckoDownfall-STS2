87cd178fa3fa314ca33749a23f16343d57ded494
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-06 20:00:04 +0800

    fix: prevent crash when cards have same rarity

--- SneckoDownfallCode/Utils/GiftSelector.cs
+++ SneckoDownfallCode/Utils/GiftSelector.cs
@@ -1,12 +1,15 @@
 
+using System.Diagnostics;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Relics;
 using MegaCrit.Sts2.Core.Runs;
+using SneckoDownfall.SneckoDownfallCode.Character;
 using SneckoDownfall.SneckoDownfallCode.Relics;
 
 namespace SneckoDownfall.SneckoDownfallCode.Utils;
@@ -15,15 +18,28 @@ public static class GiftSelector
 {
     public static IEnumerable<CardModel> GetGiftCards(Player player, Func<CardModel, bool> filter)
     {
-        var pools = player.GetRelic<SneckoSoul>()!.CharacterIds.Select(ModelDb.GetById<CharacterModel>).Select(c => c.CardPool);
+        var sneckoSoul = player.GetRelic<SneckoSoul>();
+
+        var pools =
+            (sneckoSoul == null)
+            ?
+            player.UnlockState.CharacterCardPools.Where(p => p is not SneckoDownfallCardPool)
+            :
+            sneckoSoul.CharacterIds.Select(ModelDb.GetById<CharacterModel>).Select(c => c.CardPool);
+
         var cards = pools.SelectMany(c => c.GetUnlockedCards(player.UnlockState, player.RunState.CardMultiplayerConstraint));
+
+        Log.Info($"GiftSelector: Found {cards.Count()} candidate cards for player {player} with filter {filter}");
         return cards.Where(filter);
     }
 
     public static async Task GetGiftReward(Player player, Func<CardModel, bool> filter)
     {
         var candidateCards = GetGiftCards(player, filter);
-        var options = new CardCreationOptions(candidateCards, CardCreationSource.Other, CardRarityOddsType.RegularEncounter);
+
+        var IsRarityAllTheSame = candidateCards.Select(c => c.Rarity).Distinct().Count() <= 1;
+
+        var options = new CardCreationOptions(candidateCards, CardCreationSource.Other, IsRarityAllTheSame ? CardRarityOddsType.Uniform : CardRarityOddsType.RegularEncounter);
         var options2 = (from r in CardFactory.CreateForReward(player, 3, options)
                                     select r.Card).ToList();
         var chosenCard = await CardSelectCmd.FromChooseACardScreen(new BlockingPlayerChoiceContext(), options2, player, canSkip: true);
