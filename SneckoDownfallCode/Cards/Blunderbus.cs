68901338d6128ef1688143bfb6aa92fbb58ed1be
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-07 03:45:45 +0800

    fix: incorrect `DynamicVars` name

--- SneckoDownfallCode/Cards/Blunderbus.cs
+++ SneckoDownfallCode/Cards/Blunderbus.cs
@@ -11,16 +11,17 @@ public class Blunderbus : SneckoDownfallCard
     public Blunderbus() : base(1, CardType.Attack, CardRarity.Rare, TargetType.AllEnemies)
     {
         WithDamage(8, 3);
+        WithEnergy(3);
         WithCalculatedVar("Hits", 1, CalculateBonusHits);
     }
 
     private static decimal CalculateBonusHits(CardModel card, Creature? _)
     {
-        return PileType.Hand.GetPile(card.Owner).Cards.Count(c => c.EnergyCost.Canonical >= 3);
+        return PileType.Hand.GetPile(card.Owner).Cards.Count(c => c.EnergyCost.GetResolved() >= card.DynamicVars.Energy.IntValue);
     }
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay play)
     {
-        await CommonActions.CardAttack(this, play, DynamicVars["CalculatedHits"].IntValue).Execute(ctx);
+        await CommonActions.CardAttack(this, play, DynamicVars["Hits"].IntValue).Execute(ctx);
     }
 }
